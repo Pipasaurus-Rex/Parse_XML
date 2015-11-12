ee8a5ca3ee7d0b26159ec5a3899bd327e4ea369a
Iain McDonald <IainMcDonald90@googlemail.com>
2015-11-12 06:09:26 -0800

    Data on Transcripts Added

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
 import xml.etree.ElementTree as tree
-print " "
-print "Schema Details"
 LRG_file_tree = tree.parse('LRG_517.xml')
 root=LRG_file_tree.getroot()
+
+print " "
+print "Schema Details"
 print" "
 print "Schema Version:", root.attrib['schema_version']
 for element  in LRG_file_tree.iter(tag='id'):
@@ -17,8 +18,13 @@ address = elem.find('address').text
 email= elem.find('email').text
 print "Source Name:", name
 print "Source Address:", address
-print "source E-mail:", email
+print "Source E-mail:", email
 for element  in LRG_file_tree.iter(tag='creation_date'):
     print "Creatation Date:", element.text
-    
+print" "
+tran = root.find('fixed_annotation/transcript')
+print "Transcript Name:", tran.attrib['name']
+for element in root.findall('fixed_annotation/transcript/coordinates'):
+    print "Co-ordinate system:", element.attrib['coord_system']
+     
 
