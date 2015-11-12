44ceb26a9480b024ba20161acce254ffaff443ff
Iain McDonald <IainMcDonald90@googlemail.com>
2015-11-12 03:38:22 -0800

    some child information

--- parser.py
+++ parser.py
@@ -1,7 +1,16 @@
 import xml.etree.ElementTree as tree
 print " "
-print "Schema Version"
+print "Schema Details"
 LRG_file_tree = tree.parse('LRG_517.xml')
 root=LRG_file_tree.getroot()
-print root.attrib['schema_version']
+id=root[0][0].text
+hgnc_id=root[0][1].text
+print" "
+print "Schema Version:", root.attrib['schema_version']
+print "Schema ID:",  id
+print "Schema HGNC ID:", hgnc_id
+#print "list of moons"
+#for element  in planets_tree.iter(tag='moon'):
+#    print element.attrib
+
 
