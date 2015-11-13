1b0d440b36330a5ce74090ffde0f7af4fbd674cd
Alex Henderson <alexander.henderson-4@postgrad-manchester.ac.uk>
2015-11-13 04:11:33 -0800

    XML output added to the end of the parser file Exports main details -- needs work to export the introns -- AH

--- LRG_reader.py
+++ LRG_reader.py
@@ -2,23 +2,16 @@ import xml.etree.ElementTree as tree
 LRG_file_tree = tree.parse('LRG_517.xml')
 root=LRG_file_tree.getroot()
 
-print " "
-print "Schema Details"
-print" "
-print "Schema Version:", root.attrib['schema_version']
-assert root.attrib['schema_version'] == '1.9' , 'wrong schema version'
-print "Schema ID:", root.find('fixed_annotation/id').text
-print "Schema HGNC ID:", root.find('fixed_annotation/hgnc_id').text
-print "Organism:", root.find('fixed_annotation/organism').text
-elem = root.find('fixed_annotation/source/contact')
-name = elem.find('name').text
-address = elem.find('address').text
-email= elem.find('email').text
-print "Source Name:", name
-print "Source Address:", address
-print "Source E-mail:", email
-print "Creatation Date:", root.find('fixed_annotation/creation_date').text
-print" "
+# variables
+root_id = root.find('fixed_annotation/id').text
+root_hgnc = root.find('fixed_annotation/hgnc_id').text
+root_organism = root.find('fixed_annotation/organism').text
+root_creation_date = root.find('fixed_annotation/creation_date').text
+root_source = root.find('fixed_annotation/source/contact')
+root_source_name = root_source.find('name').text
+root_source_address = root_source.find('address').text
+root_source_email= root_source.find('email').text
+
 tran = root.find('fixed_annotation/transcript')
 print "Transcript Name:", tran.attrib['name']
 for element in root.findall('fixed_annotation/transcript/coordinates'):
@@ -47,7 +40,7 @@ introns=[]
 for i in range(len(exons)+1):
 	if i==0:
 		introns.append({'intron_number':i, 'start':0, 'end':5000})
-	elif i<len(exons):
+q	elif i<len(exons):
 		introns.append({'intron_number':i, 'start':(int(exons[i-1]['end'])+1), 'end':(int(exons[i]['start'])-1)})
 	else:
 		introns.append({'intron_number':i, 'start':(int(exons[i-1]['end'])+1), 'end':len(sequence)})
@@ -60,3 +53,30 @@ for i in range(len(introns)):
 	intron_sequences.append(sequence[introns[i]['start']:introns[i]['end']])
 
 print (intron_sequences[0])
+
+
+# Export intron information to an XML file
+
+# establish root and child
+output = tree.Element("lrg_introns", schema_version = root.attrib['schema_version'])
+schema_details = tree.SubElement(output, "schema_details")
+
+# schema details
+tree.SubElement(schema_details, "id").text = root_id
+tree.SubElement(schema_details, "hgnc_id").text = root_hgnc
+tree.SubElement(schema_details, "organism").text = root_organism
+
+# new subelement
+source = tree.SubElement(schema_details, "source")
+contact_details = tree.SubElement(source, "contact_details")
+
+if root_source_name != None:
+	tree.SubElement(contact_details, "name").text = root_source_name
+	tree.SubElement(contact_details, "name").text = root_source_address
+	tree.SubElement(contact_details, "name").text = root_source_email
+	
+ 
+
+# export xml
+tree = tree.ElementTree(output)
+tree.write("alex_test_file.xml")
